23e31e129a1331bcfc0fd38878322af53da3ab2f
Zerhigh <samuel@plix.at>
2025-02-17 16:28:23 +0100

    adding querying raster creation and metadatA script

--- Query_Raster.py
+++ Query_Raster.py
@@ -2,6 +2,7 @@ import rasterio
 import numpy as np
 import geopandas as gpd
 import shapely
+import os
 from rasterio.transform import from_origin
 from shapely.geometry import box
 import json
@@ -28,23 +29,33 @@ def create_uniform_raster(polygon, raster_size, width, height):
 
     # Create raster grid
     geometries = []
+    centroids = []
     y = maxy
     while y - cell_height > miny:
         x = minx
         while x + cell_width < maxx:
             cell = box(x, y - cell_height, x + cell_width, y)
             geometries.append(cell)
+            centroids.append(cell.centroid)
             x += cell_width
         y -= cell_height
 
     # Create GeoDataFrame
-    gdf = gpd.GeoDataFrame(geometry=geometries, crs='EPSG:31287')
+    gdf = gpd.GeoDataFrame(data={'geometry': centroids, 'grid_cells': geometries}, crs='EPSG:31287')
     return gdf
 
 
-# Example usage
-polygon_geojson = shapely.box(487897, 509158, 474260, 528192)
+# Example usage,,536665
+polygon = shapely.box(516143,473853, 536665, 496558)
+# bbox = gpd.GeoDataFrame(geometry=[polygon], crs='EPSG:31287')
+# bbox.to_file("output/bbox.shp")
 
-gdf = create_uniform_raster(polygon_geojson, raster_size=1, width=512, height=512)
-gdf.to_file(f'output/raster.shp')
-print(gdf)
+BASE_PATH = r"C:\Users\PC\Coding\GeoQuery"
+metadata = gpd.read_file(os.path.join(BASE_PATH, "data", "ortho_cadastral_matched.shp"))
+
+raster_size=2.5
+uni_raster = create_uniform_raster(polygon, raster_size=2.5, width=512, height=512)
+joined = gpd.sjoin(uni_raster, metadata, how="inner")
+
+uni_raster.to_file(f'output/raster_2_5.shp')
+print(uni_raster)
