75dc56a666eebc65a50d554748eafa32ddb429de
Zerhigh <samuel@plix.at>
2025-02-18 11:02:11 +0100

    rasterizing works, fixed binary image

--- vector_gdal.py
+++ vector_gdal.py
@@ -18,7 +18,6 @@ from shapely.geometry import box
 
 
 def query_cadastral_data_tiled(gdf, geo_url, parameters):
-    # test
     """Fetches feature count within a bounding box using remote access."""
 
     layer_name = "NFL"
@@ -27,38 +26,43 @@ def query_cadastral_data_tiled(gdf, geo_url, parameters):
     src = f"/vsicurl/{geo_url}"
     dst = "output/tmp.gpkg"
 
-    # command_sql = f'ogr2ogr -f "GPKG" -spat {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]} -dialect OGRSQL -sql "SELECT * FROM {layer_name} WHERE NS=41" {dst} {src}'
-    # run_cmd(command_sql)
+    command_sql = f'ogr2ogr -f "GPKG" -spat {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]} -dialect OGRSQL -sql "SELECT * FROM {layer_name} WHERE NS=41" {dst} {src}'
+    run_cmd(command_sql)
 
     sub = gpd.read_file(dst)
     for ind, row in tqdm(gdf.iterrows()):
 
-        output_file = f'output_vector/{row["index"]}.gpkg'
-        # if os.path.exists(output_file):
-        #     print(f'This geometry area: {output_file} has alReady been queryied, for this timestamp.')
-        #     continue
+        clipped = None
 
-        clipped = sub.clip(mask=row.geometry, keep_geom_type=True)
-        clipped.to_file(output_file, driver='GPKG', layer='NFL')
+        output_gpkg = f'output_vector/{row["index"]}.gpkg'
+        if os.path.exists(output_gpkg):
+            print(f'This geometry area: {output_gpkg} has alReady been queryied, for this timestamp.')
+        else:
+            clipped = sub.clip(mask=row.geometry, keep_geom_type=True)
+            clipped.to_file(output_gpkg, driver='GPKG', layer='NFL')
 
-        rasterize = True
-
-        if rasterize:
+        rasterize_ = True
+        output_tif = f"output_raster_mask/{row['index']}.tif"
+        if os.path.exists(output_tif) and rasterize_:
+            print(f'This geometry area: {output_tif} has alReady been vectorized.')
+        else:
             bounds = row.geometry.bounds
             width = int((bounds[2] - bounds[0]) / parameters["pixel_size"])
             height = int((bounds[3] - bounds[1]) / parameters["pixel_size"])
 
             transform = rasterio.transform.from_origin(bounds[0], bounds[3], parameters["pixel_size"], parameters["pixel_size"])
-            binary_raster = np.zeros((height, width), dtype=np.uint8)
+
+            if clipped is None:
+                clipped = gpd.read_file(output_gpkg)
 
             # Rasterize the geometries into the raster
-            shapes = ((geom, 1) for geom in gdf.geometry)  # Assign value 1 to features
-            binary_raster = rasterize(shapes, out_shape=(height, width), transform=transform, fill=0, dtype=np.uint8)
+            shapes = [(geom, 1) for geom in clipped.geometry]  # Assign value 1 to features
+            binary_raster = rasterize(shapes, out_shape=(height, width), transform=transform, fill=0)
 
             # Save the rasterized binary image
             with rasterio.open(
-                    "binary_raster.tif",
-                    "w",
+                    fp=output_tif,
+                    mode="w+",
                     driver="GTiff",
                     height=height,
                     width=width,
@@ -67,9 +71,7 @@ def query_cadastral_data_tiled(gdf, geo_url, parameters):
                     crs=gdf.crs,
                     transform=transform
             ) as dst:
-                dst.write(binary_raster, 1)  # Write binary raster to band 1
-
-
+                dst.write(binary_raster, 1)
     return
 
 
@@ -94,5 +96,3 @@ for geoUrl, group in time_grouped:
     query_cadastral_data_tiled(gdf=group, geo_url=geoUrl, parameters=parameters)
     t2 = time.time()
     print('... took: ', t2-t1)
-
-    break
